7fa42cebb9e6419416da46a137707808dc52e31a
Arindam Pal <arindampal2000@gmail.com>
2022-08-13 11:08:36 +0530

    basic animation
    - moving snail right to left on the screen
    - making the snail loop
    - clearing the previous screen using background imagesf
    - convert png to pygame image (with alpha separate function)

--- main.py
+++ main.py
@@ -14,9 +14,12 @@ clock = pygame.time.Clock()
 # font
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
-text_surface = test_font.render('My game', False, 'Black')
+sky_surface: pygame.Surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface: pygame.Surface = pygame.image.load('graphics/ground.png').convert()
+text_surface: pygame.Surface = test_font.render('My game', False, 'Black')
+
+snail_surface: pygame.Surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_x_pos: int = 600
 
 while True:
     # draw all our elements
@@ -36,6 +39,11 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
+
+    snail_x_pos -= 4
+    if snail_x_pos < -100:
+        snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos, 250))
 
     # updates the created display surface
     pygame.display.update()
